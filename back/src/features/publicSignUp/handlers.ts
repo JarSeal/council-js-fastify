3a9c6b0d6fa1a3c26f554d2a345f01947951c031
JarSeal <kai.forsman@gmail.com>
2023-06-26 00:33:31 +0200

    Add mongoose and initDB

--- back/src/features/publicSignUp/handlers.ts
+++ back/src/features/publicSignUp/handlers.ts
@@ -1,3 +1,4 @@
 import type { RouteHandler } from 'fastify';
+// import { Mongoose } from 'mongoose';
 
 export const publicSignUp: RouteHandler = async (_, res) => res.status(200).send({ ok: true });
