2cea3079caac26b249a0a20933b8f6c7e7edfebf
JarSeal <kai.forsman@gmail.com>
2023-11-27 18:28:20 +0100

    More tests

--- back/src/utils/timeAndDate.test.ts
+++ back/src/utils/timeAndDate.test.ts
@@ -1,4 +1,4 @@
-import { getTimestamp } from './timeAndDate';
+import { getTimestamp, getTimestampFromDate } from './timeAndDate';
 
 describe('timeAndDate utils', () => {
   it('getTimestamp', () => {
@@ -6,5 +6,10 @@ describe('timeAndDate utils', () => {
     expect(timestamp).toBeGreaterThan(Math.floor(Date.now() / 1000) - 2);
     expect(timestamp).toBeLessThan(Math.floor(Date.now() / 1000) + 2);
     expect(typeof timestamp === 'number').toBeTruthy();
+  });
+
+  it('getTimestampFromDate', () => {
+    const timestamp = getTimestampFromDate(new Date('2023-11-27T09:32:29.611Z'));
+    expect(timestamp).toBe(1701077549);
   });
 });
