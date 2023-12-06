7ecc5fa11803f8d9269131e19d9c299a26c4a51d
JarSeal <kai.forsman@gmail.com>
2023-12-06 23:34:06 +0100

    FormData POST route tests

--- back/src/features/formData/routes.POST.test.ts
+++ back/src/features/formData/routes.POST.test.ts
@@ -5,6 +5,7 @@ import initApp from '../../core/app';
 import { createForm, createUser, csrfHeader, validAgentId } from '../../test/utils';
 import { SESSION_COOKIE_NAME } from '../../core/config';
 import type { FormDataPostReply } from './routes';
+import type { PublicPrivilegeProp } from '../../dbModels/_modelTypePartials';
 
 describe('POST formData', () => {
   let app: FastifyInstance;
@@ -243,11 +244,262 @@ describe('POST formData', () => {
     });
     const body = JSON.parse(response.body) as FormDataPostReply;
     expect(response.statusCode).toBe(403);
+    expect(body.ok).toBeFalsy();
     expect(body.error?.errorId).toBe('maxDataCreatorDocs');
   });
 
-  // Check form elems' privileges (create) for the sent elems
-  // Validate formData values against form elems
-  // Save partial formData
-  // Save full formData
+  it('should fail when trying to save elem data that the user does not have privileges to do so', async () => {
+    const elemPrivs = {
+      public: 'false' as PublicPrivilegeProp,
+      requireCsrfHeader: false,
+      users: [],
+      groups: [],
+      excludeUsers: [],
+      excludeGroups: [],
+    };
+    await createForm(
+      'myform',
+      '/myform',
+      [
+        {
+          elemId: 'testElem0',
+          orderNr: 0,
+          elemType: 'inputText',
+          valueType: 'string',
+          privileges: {
+            create: elemPrivs,
+            read: elemPrivs,
+            edit: elemPrivs,
+            delete: elemPrivs,
+          },
+        },
+        {
+          elemId: 'testElem1',
+          orderNr: 1,
+          elemType: 'inputNumber',
+          valueType: 'number',
+        },
+      ],
+      [
+        {
+          priCategoryId: 'form',
+          priTargetId: 'myform',
+          priAccessId: 'canUseForm',
+          privilegeAccess: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+      ],
+      {
+        formDataDefaultPrivileges: {
+          create: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+        maxDataCreatorDocs: 1,
+      }
+    );
+
+    const response = await app.inject({
+      method: 'POST',
+      path: '/api/v1/myform',
+      body: {
+        formData: [
+          {
+            elemId: 'testElem0',
+            value: 'some string',
+          },
+          {
+            elemId: 'testElem1',
+            value: 15,
+          },
+        ],
+      },
+    });
+    expect(response.statusCode).toBe(401);
+  });
+
+  it('should fail when validation for formData values fails', async () => {
+    await createForm(
+      'myform',
+      '/myform',
+      [
+        {
+          elemId: 'testElem0',
+          orderNr: 0,
+          elemType: 'inputText',
+          valueType: 'string',
+        },
+        {
+          elemId: 'testElem1',
+          orderNr: 1,
+          elemType: 'inputNumber',
+          valueType: 'number',
+        },
+      ],
+      [
+        {
+          priCategoryId: 'form',
+          priTargetId: 'myform',
+          priAccessId: 'canUseForm',
+          privilegeAccess: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+      ],
+      {
+        formDataDefaultPrivileges: {
+          create: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+        maxDataCreatorDocs: 1,
+      }
+    );
+
+    const response = await app.inject({
+      method: 'POST',
+      path: '/api/v1/myform',
+      body: {
+        formData: [
+          {
+            elemId: 'testElem0',
+            value: 15,
+          },
+          {
+            elemId: 'testElem1',
+            value: 'some string',
+          },
+        ],
+      },
+    });
+    const body = JSON.parse(response.body) as FormDataPostReply;
+    expect(response.statusCode).toBe(400);
+    expect(body.ok).toBeFalsy();
+    expect(body.error?.errorId).toBe('invalidValueType');
+  });
+
+  it('should save partial formData', async () => {
+    await createForm(
+      'myform',
+      '/myform',
+      [
+        {
+          elemId: 'testElem0',
+          orderNr: 0,
+          elemType: 'inputText',
+          valueType: 'string',
+          required: true,
+        },
+        {
+          elemId: 'testElem1',
+          orderNr: 1,
+          elemType: 'inputNumber',
+          valueType: 'number',
+        },
+      ],
+      [
+        {
+          priCategoryId: 'form',
+          priTargetId: 'myform',
+          priAccessId: 'canUseForm',
+          privilegeAccess: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+      ],
+      {
+        formDataDefaultPrivileges: {
+          create: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+        maxDataCreatorDocs: 1,
+      }
+    );
+
+    const response = await app.inject({
+      method: 'POST',
+      path: '/api/v1/myform',
+      body: {
+        formData: [
+          {
+            elemId: 'testElem0',
+            value: 'some string',
+          },
+        ],
+      },
+    });
+    const body = JSON.parse(response.body) as FormDataPostReply;
+    expect(response.statusCode).toBe(200);
+    expect(body.ok).toBeTruthy();
+  });
+
+  it('should save full formData', async () => {
+    await createForm(
+      'myform',
+      '/myform',
+      [
+        {
+          elemId: 'testElem0',
+          orderNr: 0,
+          elemType: 'inputText',
+          valueType: 'string',
+          required: true,
+        },
+        {
+          elemId: 'testElem1',
+          orderNr: 1,
+          elemType: 'inputNumber',
+          valueType: 'number',
+        },
+      ],
+      [
+        {
+          priCategoryId: 'form',
+          priTargetId: 'myform',
+          priAccessId: 'canUseForm',
+          privilegeAccess: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+      ],
+      {
+        formDataDefaultPrivileges: {
+          create: {
+            public: 'true',
+            requireCsrfHeader: false,
+          },
+        },
+        maxDataCreatorDocs: 1,
+      }
+    );
+
+    const response = await app.inject({
+      method: 'POST',
+      path: '/api/v1/myform',
+      body: {
+        formData: [
+          {
+            elemId: 'testElem0',
+            value: 'some string',
+          },
+          {
+            elemId: 'testElem1',
+            value: 12,
+          },
+        ],
+      },
+    });
+    const body = JSON.parse(response.body) as FormDataPostReply;
+    expect(response.statusCode).toBe(200);
+    expect(body.ok).toBeTruthy();
+  });
 });
