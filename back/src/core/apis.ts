6149db9f1aa5862c99eaac2ac9a16c67afb51f3e
JarSeal <kai.forsman@gmail.com>
2024-01-17 07:40:31 +0100

    Fix and refactor apis and add systemSettings route

--- back/src/core/apis.ts
+++ back/src/core/apis.ts
@@ -5,12 +5,12 @@ import publicSignUpRoute from '../features/publicSignUp/routes';
 import loginRoutes from '../features/login/routes';
 import logoutRoute from '../features/logout/routes';
 import formDataRoutes from '../features/formData/routes';
+import systemSettingsRoute from '../features/systemSettings/routes';
 import { notSignedInHook } from '../hooks/notSignedIn';
-// import { signedInHook } from '../hooks/signedIn';
+import { signedInHook } from '../hooks/signedIn';
 import { csrfHook } from '../hooks/csrf';
 
 export const apiVersion = '/v1';
-const vPrefixObj = { prefix: apiVersion };
 const sysPrefixObj = { prefix: apiVersion + '/sys' };
 
 // All API routes:
@@ -23,28 +23,28 @@ const apis: FastifyPluginAsync = async (instance) => {
 const publicRoutes: FastifyPluginAsync = async (instance) => {
   await instance.register(formDataRoutes);
   await instance.register(healthCheckRoutes, sysPrefixObj);
-  await instance.register(logoutRoute, vPrefixObj);
+  await instance.register(logoutRoute, sysPrefixObj);
 };
 
 // All state altering system API routes (check CSRF header)
 const stateAlteringSystemRoutes: FastifyPluginAsync = async (instance) => {
   instance.addHook('onRequest', csrfHook);
   await instance.register(notSignedInSystemRoutes);
-  // await instance.register(signedInSystemRoutes);
+  await instance.register(signedInSystemRoutes);
 };
 
 // Not signed in system API routes:
 const notSignedInSystemRoutes: FastifyPluginAsync = async (instance) => {
   instance.addHook('onRequest', notSignedInHook);
-  await instance.register(publicSignUpRoute, vPrefixObj);
-  await instance.register(loginRoutes, vPrefixObj);
+  await instance.register(publicSignUpRoute, sysPrefixObj);
+  await instance.register(loginRoutes, sysPrefixObj);
 };
 
 // Signed in system API routes:
 // *****************
-// const signedInSystemRoutes: FastifyPluginAsync = async (instance) => {
-//   instance.addHook('onRequest', signedInHook);
-//   // @TODO: add system settings route
-// };
+const signedInSystemRoutes: FastifyPluginAsync = async (instance) => {
+  instance.addHook('onRequest', signedInHook);
+  await instance.register(systemSettingsRoute, sysPrefixObj);
+};
 
 export default apis;
