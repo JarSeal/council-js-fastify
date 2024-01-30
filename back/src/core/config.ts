9f29e72ffc09da6744ac487934ddbeac4c3fc04f
JarSeal <kai.forsman@gmail.com>
2024-01-30 23:31:11 +0100

    Add array check

--- back/src/core/config.ts
+++ back/src/core/config.ts
@@ -89,7 +89,7 @@ export const getSysSetting = async <T>(id: string): Promise<T | undefined> => {
 
   const setting = settings.find((item) => item.simpleId === id);
   if (!setting) {
-    const formElem = cachedSysSettingsForm?.form.formElems
+    const formElem = Array.isArray(cachedSysSettingsForm?.form.formElems)
       ? cachedSysSettingsForm?.form.formElems.find((elem) => elem.elemId === id)
       : undefined;
     return formElem?.elemData?.defaultValue as T;
