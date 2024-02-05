54f2ba9075a9504115c1b63aa56ab4c5d9eaa561
JarSeal <kai.forsman@gmail.com>
2024-02-05 20:46:49 +0100

    Use only one migration file for init data

--- back/migrate/migrations/00000000000000-init-data.js
+++ back/migrate/migrations/00000000000000-init-data.js
@@ -1,14 +1,65 @@
+const {
+  getSuperAdminUsername,
+  getSuperAdminPassword,
+  getSuperAdminEmail,
+} = require('../data/utils');
+const systemGroups = require('../data/system-groups');
 const getSystemForms = require('../data/system-forms');
-const { getSuperAdminUsername } = require('../data/utils');
 const { encryptData } = require('../../dist/back/src/core/config');
 
 module.exports = {
+  // UP
+  // **********************************************
   async up(db) {
+    const timeNow = new Date();
+
+    // Create superadmin
+    await db.collection('users').insertOne({
+      simpleId: getSuperAdminUsername(),
+      emails: [
+        {
+          email: getSuperAdminEmail(),
+          verified: true,
+          added: timeNow,
+        },
+      ],
+      passwordHash: await getSuperAdminPassword(),
+      created: {
+        user: null,
+        publicForm: false,
+        date: timeNow,
+      },
+      edited: [],
+      systemDocument: true,
+      security: {
+        forcePassChange: false,
+        loginAttempts: 0,
+        coolDownStarted: null,
+        isUnderCoolDown: false,
+        lastLoginAttempts: [],
+        lastLogins: [],
+      },
+    });
+
     // Get superadmin
     const superUser = await db.collection('users').findOne({ simpleId: getSuperAdminUsername() });
-    let privileges = [];
+
+    // Create system groups
+    for (let i = 0; i < systemGroups.length; i++) {
+      const foundGroup = await db
+        .collection('groups')
+        .findOne({ simpleId: systemGroups[i].simpleId });
+      if (!foundGroup) {
+        // Add super user as the creator and owner (owner is important)
+        systemGroups[i].created.user = superUser._id;
+        systemGroups[i].owner = superUser._id;
+        systemGroups[i].members = [superUser._id];
+        await db.collection('groups').insertOne(systemGroups[i]);
+      }
+    }
 
     // Create system forms
+    let privileges = [];
     const systemForms = await getSystemForms(db);
     for (let i = 0; i < systemForms.length; i++) {
       const foundForm = await db.collection('forms').findOne({ simpleId: systemForms[i].simpleId });
@@ -67,7 +118,10 @@ module.exports = {
     }
   },
 
+  // DOWN (in reverse order)
+  // **********************************************
   async down(db) {
+    // Remove system forms
     let privileges = [];
     const systemForms = await getSystemForms(db);
     for (let i = 0; i < systemForms.length; i++) {
@@ -77,5 +131,13 @@ module.exports = {
     for (let i = 0; i < privileges.length; i++) {
       await db.collection('privileges').deleteOne({ simpleId: privileges[i].simpleId });
     }
+
+    // Remove system groups
+    for (let i = 0; i < systemGroups.length; i++) {
+      await db.collection('groups').deleteOne({ simpleId: systemGroups[i].simpleId });
+    }
+
+    // Remove superadmin
+    await db.collection('users').deleteOne({ simpleId: getSuperAdminUsername() });
   },
 };
