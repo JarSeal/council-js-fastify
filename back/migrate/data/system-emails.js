024424d855272aab65eeb414bb42c60c3d49597a
JarSeal <kai.forsman@gmail.com>
2024-02-06 22:20:49 +0100

    Basic system email templates migration

--- back/migrate/data/system-emails.js
+++ back/migrate/data/system-emails.js
@@ -76,10 +76,10 @@ const getEmails = () => {
     {
       simpleId: 'welcomeEmail',
       name: { langKey: 'Welcome email' },
-      description: { langKey: 'Welcome email for newly registered users.' },
+      description: { langKey: 'Welcome email template for newly registered users.' },
       systemDocument: true,
       wrapperTemplateId: 'mainSysEmailWrapper',
-      templateVarKeys: ['appName', 'username', 'sysLoginUrl', 'newPassRequestUrl'],
+      templateVarKeys: ['username', 'sysLoginUrl', 'newPassRequestUrl'],
       subject: 'Welcome to {{appName}}',
       template: `
 Welcome
@@ -91,10 +91,136 @@ Your new account has been registered:
 You can either [login]({{sysLoginUrl}}) or if you don't have a password,  
 you can [create a new password]({{newPassRequestUrl}}) for you.
 
+Login URL: {{sysLoginUrl}}
+Create a new password URL: {{newPassRequestUrl}}
+
 Do not reply to this email, thank you.
       `,
     },
     // Welcome email [/END]
+
+    // Verify email [START]
+    {
+      simpleId: 'verifyEmail',
+      name: { langKey: 'Verify email' },
+      description: {
+        langKey: "Verify user's primary email template that has the tokenized verification URL.",
+      },
+      systemDocument: true,
+      wrapperTemplateId: 'mainSysEmailWrapper',
+      templateVarKeys: ['username', 'verifyEmailUrl'],
+      subject: 'Verify account email',
+      template: `
+Almost there, {{username}}, please verify your E-mail
+-----------------------------------------------------
+
+Please verify your account's E-mail by clicking this link:
+[VERIFY]({{verifyEmailUrl}})
+
+..or copy and paste this URL to your browser:
+{{verifyEmailUrl}}
+
+Do not reply to this email, thank you.
+      `,
+    },
+    // Verify email [/END]
+
+    // New pass link email [START]
+    {
+      simpleId: 'newPassLinkEmail',
+      name: { langKey: 'New password link email' },
+      description: {
+        langKey: 'New password link email template that has the tokenized new password URL.',
+      },
+      systemDocument: true,
+      wrapperTemplateId: 'mainSysEmailWrapper',
+      templateVarKeys: ['username', 'newPassUrl', 'linkLifeInMinutes'],
+      subject: 'Reset password link',
+      template: `
+Your Reset Password Link is Ready, {{username}}
+-----------------------------------------------
+
+You requested a link to reset your password. Here you go:
+[Reset my password]({{newPassUrl}})
+
+..or copy and paste this URL to your browser:
+{{newPassUrl}}
+
+If you did not request this link, you can ignore this (only you have this message). This link will expire in {{linkLifeInMinutes}} minutes. If you suspect that someone is trying to access your account, please contact the system administrator.
+
+Do not reply to this email, thank you.
+      `,
+    },
+    // New pass link email [/END]
+
+    // Pass changed notification email [START]
+    {
+      simpleId: 'passChangedEmail',
+      name: { langKey: 'Password changed notification email' },
+      description: { langKey: "Notification email template for a user's password change." },
+      systemDocument: true,
+      wrapperTemplateId: 'mainSysEmailWrapper',
+      templateVarKeys: ['username'],
+      subject: 'Your password has been changed',
+      template: `
+Your password was changed, {{username}}
+---------------------------------------
+
+This is a notification that your password was changed.
+
+Do not reply to this email, thank you.
+      `,
+    },
+    // Pass changed notification email [/END]
+
+    // 2FA code email [START]
+    {
+      simpleId: '2FACodeEmail',
+      name: { langKey: '2FA code email' },
+      description: { langKey: '2-factor authorization email that has the code to login.' },
+      systemDocument: true,
+      wrapperTemplateId: 'mainSysEmailWrapper',
+      templateVarKeys: ['twoFactorCode', 'twoFactorLifeInMinutes'],
+      subject: 'Login Code [{{twoFactorCode}}]',
+      template: `
+Welcome back!
+-------------
+
+Here is your 2FA Login Code:
+
+### {{twoFactorCode}}
+
+The Login Code (__{{twoFactorCode}}__) is valid for __{{twoFactorLifeInMinutes}}__ minutes and can be only used once.
+
+If you are not trying to log in, you can ignore this (only you have this message). If you suspect that someone is trying to access your account, please contact the system administrator.
+
+Do not reply to this email, thank you.
+      `,
+    },
+    // 2FA code email [/END]
+
+    // Delete own account email [START]
+    {
+      simpleId: 'deleteOwnAccountEmail',
+      name: { langKey: 'Delete own account email' },
+      description: { langKey: 'Email to send to user after they have deleted their own account.' },
+      systemDocument: true,
+      wrapperTemplateId: 'mainSysEmailWrapper',
+      templateVarKeys: ['username'],
+      subject: 'Account deleted',
+      template: `
+Goodbye
+-------
+
+Your account and your data has been deleted.
+- Username: {{username}}
+
+You have deleted your own account. Thank you for being a user with us!
+
+Do not reply to this email, thank you.
+      `,
+    },
+    // Delete own account email [/END]
   ];
 };
 
