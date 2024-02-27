82b86cec98ac92b5746712e902246b1984b15605
JarSeal <kai.forsman@gmail.com>
2024-02-27 08:59:16 +0100

    Remove api health check monitoring and modify config

--- back/src/features/healthCheck/handlers.ts
+++ back/src/features/healthCheck/handlers.ts
@@ -3,12 +3,7 @@ import type { RouteHandler } from 'fastify';
 import type { HealthCheckRoute } from './routes';
 import { addMonitorCount } from '../../utils/monitorUtils';
 
-export const healthCheck: RouteHandler<HealthCheckRoute> = async (_, res) => {
-  const addMonitorResult = await addMonitorCount('apiHealthCheck');
-  if (addMonitorResult) return res.send(addMonitorResult);
-
-  return res.send({ ok: true });
-};
+export const healthCheck: RouteHandler<HealthCheckRoute> = async (_, res) => res.send({ ok: true });
 
 export const healthCheckDB: RouteHandler<HealthCheckRoute> = async (_, res) => {
   const addMonitorResult = await addMonitorCount('dbHealthCheck');
